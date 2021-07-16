3b804d2d6cf47bfcff56c401e0eb94ef4cfeb976
olaar09 <agboolar09@gmail.com>
2021-07-16 15:07:43 +0100

    ..

--- lib/logic/meta_bloc/metadata_bloc.dart
+++ lib/logic/meta_bloc/metadata_bloc.dart
@@ -3,32 +3,22 @@ import 'package:acceptwire/repository/auth_repository.dart';
 import 'package:acceptwire/repository/meta_repository.dart';
 import 'package:acceptwire/logic/meta_bloc/metadata_event.dart';
 import 'package:acceptwire/logic/meta_bloc/metadata_state.dart';
-import 'package:acceptwire/utils/helpers/helpers.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MetaDataBloc extends Bloc<MetaDataEvent, MetaDataState> {
+class MetaDataBloc extends Cubit<MetaDataState> {
   MetaDataRepo metaDataRepo;
   AuthRepository authRepository;
 
   MetaDataBloc({required this.metaDataRepo, required this.authRepository})
       : super(MetaDataLoadingState());
 
-  @override
-  Stream<MetaDataState> mapEventToState(MetaDataEvent event) async* {
-    if (event is MetaDataLoadingEvent) yield* _mapToMetaLoading();
-  }
-
-  void fireLoadingEvent() {
-    this.add(MetaDataLoadingEvent());
-  }
-
-  Stream<MetaDataState> _mapToMetaLoading() async* {
-    yield MetaDataLoadingState();
+  void fireLoadingEvent() async {
+    this.emit(MetaDataLoadingState());
     var metaResponse = await metaDataRepo.fetchMetaData();
     if (metaResponse is AppConfig) {
-      yield MetaDataLoadedState(appConfig: metaResponse);
+      this.emit(MetaDataLoadedState(appConfig: metaResponse));
     } else {
-      yield MetaDataErrorState(message: '$metaResponse');
+      this.emit(MetaDataErrorState(message: '$metaResponse'));
     }
   }
 }
