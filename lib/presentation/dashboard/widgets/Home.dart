43917c3ed08118acbac011fb5704152c8ff89160
olaar09 <agboolar09@gmail.com>
2021-07-18 23:00:28 +0100

    can sign up

--- lib/presentation/dashboard/widgets/Home.dart
+++ lib/presentation/dashboard/widgets/Home.dart
@@ -1,11 +1,8 @@
-import 'package:acceptwire/logic/fetch_balance/fetch_balance_bloc.dart';
 import 'package:acceptwire/presentation/dashboard/widgets/AccountBalance.dart';
 import 'package:acceptwire/presentation/dashboard/widgets/AccountNumbers.dart';
 import 'package:acceptwire/presentation/dashboard/widgets/LatestTransactions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:focus_detector/focus_detector.dart';
 
 class Home extends StatefulWidget {
   @override
--- lib/presentation/dashboard/widgets/LatestTransactions.dart
+++ lib/presentation/dashboard/widgets/LatestTransactions.dart
@@ -1,5 +1,6 @@
 import 'package:acceptwire/utils/helpers/text.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class LatestTransactions extends StatelessWidget {
   const LatestTransactions({
@@ -19,23 +20,41 @@ class LatestTransactions extends StatelessWidget {
               child: regularText('Latest transactions'),
             ),
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: CustomScrollView(
-                  physics: BouncingScrollPhysics(),
-                  slivers: [
-                    CupertinoSliverRefreshControl(
-                      onRefresh: () async {
-                        await Future.delayed(Duration(seconds: 20));
-                      },
-                    ),
-                    SliverList(
-                      delegate: SliverChildBuilderDelegate((context, index) {
-                        return Text('Transaction item');
-                      }, childCount: 100),
-                    ),
-                  ],
-                ),
+              child: CustomScrollView(
+                physics: BouncingScrollPhysics(),
+                slivers: [
+                  CupertinoSliverRefreshControl(
+                    onRefresh: () async {
+                      await Future.delayed(Duration(seconds: 20));
+                    },
+                  ),
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate((context, index) {
+                      return Container(
+                        color: Colors.white,
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              height: 60,
+                              child: Row(
+                                children: [
+                                  CircleAvatar(
+                                    child: Text('bnk'),
+                                    radius: 16,
+                                  ),
+                                  SizedBox(width: 10),
+                                  Text('Transaction item'),
+                                ],
+                              ),
+                            ),
+                            Divider()
+                          ],
+                        ),
+                      );
+                    }, childCount: 100),
+                  ),
+                ],
               ),
             ),
           ],
